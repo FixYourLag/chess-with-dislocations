00d4ee4a1dc5abd262e67adf1531fcd56533b4f1
maddy <michaelmurrell1122@gmail.com>
2022-12-09 15:01:26 +0000

    add game class

---
+++ chess-with-dislocations/Game.cs
@@ -0,0 +1,30 @@
+using 
+namespace chess_with_dislocations;
+
+public class Game
+{
+    public Game()
+    {
+        //
+    }
+    bool isWhiteTurn = true;
+    Board board = new Board();
+
+
+    public bool isMoveLegal(int x1, int y1, int x2, int y2)
+    {
+        Pieces[,] piecesArray = new Pieces[23,23];
+        Pieces startPiece = piecesArray[x1, y1];
+        Pieces endPiece = piecesArray[x2, y2];
+        if (startPiece.GetType() == '#')
+        {
+            return false;
+        }
+        if (startPiece.IsWhite() != isWhiteTurn)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
